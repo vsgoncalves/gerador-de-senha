7a4ecf12e4728c32e704f94838fe6ebf458636ec
Vinicius Goncalves <vinicius.souza.goncalves@gmail.com>
2023-07-31 15:08:19 -0300

    agora pode mudar a visualização da quantidade de caracteres do compo tamanho e modificara o tamamnho da senha que será gerada

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { useState } from "react"
 function App() {
   const [password, setPassword] = useState("")
 	const [copyText, setCopyText] = useState("Copiar")
+  const [passwordSize, setPasswordSize] = useState(12)
+  const [showInput, setShowInput] = useState(false)
+  
 
 	function generate() {
     const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 12
+    
     let newPassword = ""
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const position = Math.floor(Math.random() * characters.length)
       newPassword += characters[position]
     }
@@ -24,7 +27,31 @@ function App() {
   return (
     <div>
       <h1>Gerador de senhas</h1>
-			<button onClick={generate}>Gerar!</button>
+      <div>
+        <label htmlFor="showInput">Customizar tamanho da senha:</label>
+        <input
+         type="checkbox" 
+         id="showInput"
+         value={showInput}
+         onChange={() => setShowInput(currentState => !currentState)}
+         />
+      </div>
+      {showInput ? (
+        <div>
+        <label htmlFor="passwordSize">Tamanho:</label>
+        <input 
+          type="number"
+          id="passwordSize"
+          min={1}
+          value={passwordSize}
+          onChange={(ev) => setPasswordSize(ev.target.value)}
+        />
+      </div>
+
+      ) : null}
+
+      
+			<button onClick={generate}>Gerar senha de {showInput ? passwordSize : 12} caracteres!</button>
 			<button onClick={copyToClipboard}>{copyText}</button>
       <div>{password}</div>
     </div>
